6a68565c20ff1f74d90c32053545446a9071d52b
josedhonatas <dhonatas@academico.ufs.br>
2023-04-20 21:39:09 -0300

    uiiui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import MaterialTable from '@material-table/core';
+
 function App() {
+  
+  const [data, setData] = useState([]);
+  const [columns, setColumns] = useState([]);
+
+  useEffect(() => {
+    fetch('https://sisa-api-nalwq.ondigitalocean.app/usuarios/')
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+        setColumns([
+          { title: "Name", field: "nome_completo" },
+          { title: "Email", field: "email" },
+          { title: "Phone Number", field: 'telefone' },
+          { title: "Department", field: 'departamento' },
+        ]);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 align="center">React-App</h1>
+      <h4 align='center'>CRUD com usuarios</h4>
+      <MaterialTable
+        title="Usuarios"
+        data={data}
+        columns={columns}
+        editable={{
+          onRowAdd: (newRow) => new Promise((resolve, reject) => {
+            const updatedRows = [...data, { id: Math.floor(Math.random() * 100), ...newRow }]
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          }),
+          onRowDelete: selectedRow => new Promise((resolve, reject) => {
+            const id = selectedRow.id;
+            const index = data.findIndex(row => row.id === id);
+            const updatedRows = [...data];
+            updatedRows.splice(index, 1);
+            setTimeout(() => {
+              setData(updatedRows);
+              resolve();
+            }, 2000);
+          }),          
+          onRowUpdate:(updatedRow,oldRow)=>new Promise((resolve,reject)=>{
+            const index=oldRow.tableData.id;
+            const updatedRows=[...data]
+            updatedRows[index]=updatedRow
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          })
+
+        }}
+        options={{
+          actionsColumnIndex: -1, addRowPosition: "first"
+        }}
+      />
     </div>
   );
 }
